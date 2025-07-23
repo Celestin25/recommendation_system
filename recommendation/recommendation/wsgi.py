8cddfa859a8957a4bfb04701662eec7a1d17ea90
Celestin25 <celestinhakorimana25@gmail.com>
2025-07-23 11:21:59 +0200

    wsgi imported

--- recommendation/recommendation/wsgi.py
+++ recommendation/recommendation/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recommendation.setting')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recommendation.settings')
 
 application = get_wsgi_application()
