8bc55d2f10bfbf5a90011184e3a80876e1c46040
Celestin25 <celestinhakorimana25@gmail.com>
2025-07-30 14:10:02 +0200

    request parameter

--- app/projects/permissions.py
+++ app/projects/permissions.py
@@ -2,5 +2,5 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsInvitee(BasePermission):
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, reques, view, obj):
         return obj.invitee_email == request.user.email
