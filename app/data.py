a3e3f2f945bfa1daed3a1cd9fe0423fe88f845ed
Celestin25 <celestinhakorimana25@gmail.com>
2025-08-26 15:37:04 +0200

    fruits

--- app/data.py
+++ app/data.py
@@ -1,10 +1,4 @@
-numbers = [1, 3, 3, 5, 6]
-fruits = ['Apples', 'Oranges', 'Grapes']
-print(len(fruits))
-fruits.append('Mangos')
-fruits.remove("Mangos")
-print(fruits)
-fruits.reverse()
-print(fruits)
-fruits.insert(2, "Strawberries")
+
+fruits = {'Apples', 'Oranges', 'Mango'}
+fruits.add('Avocado')
 print(fruits)
