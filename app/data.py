e507b4785bcdeb6fca9f5b441c7ec7d075b513d7
Celestin25 <celestinhakorimana25@gmail.com>
2025-08-26 15:12:26 +0200

    list of names

--- app/data.py
+++ app/data.py
@@ -1,4 +1,7 @@
-x = 1
-y = 3.5
-name = 'John'
-is_cool = True
+x, y, name, is_cool = (1, 2.5, "John", True)
+
+print(x, y, name, is_cool)
+x_type = type(x)
+print(x_type)
+name = 'Celestin '
+print(f"My is {name}")
